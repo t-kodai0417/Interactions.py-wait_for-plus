ba8dfadc7e8332c68f2beed38ad400d156ad2d87
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-07-16 23:49:36 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='interactions_wait_for_plus',
     version='0.1',
-    packages=find_packages()
+    packages=["interactions.ext.wait_for_plus"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["discord-py-interactions>=4.1.0"],
 )
