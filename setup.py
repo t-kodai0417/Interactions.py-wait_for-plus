674f18fe60d69c7727e19e84c233dcbf553abefc
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-07-16 23:46:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Interactions_wait_for_plus',
+    name='interactions_wait_for_plus',
     version='0.1',
     packages=find_packages()
 )
