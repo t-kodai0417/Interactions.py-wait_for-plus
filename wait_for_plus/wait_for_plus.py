7afd09ddcf5c6e82da663a59f354e75307ea51a7
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-07-17 12:56:16 +0900

    buttonをadd

--- wait_for_plus/wait_for_plus.py
+++ wait_for_plus/wait_for_plus.py
@@ -13,4 +13,18 @@ class select():
         return self.ctx._json["member"]["roles"]
     def channel_id(self) -> int:
         return int(self.ctx._json["channel_id"])
+
+class button():
+    def __init__(self, ctx):
+        self.ctx = ctx
+    def custom_id(self) -> str:
+        return self.ctx._json["data"]["custom_id"]
+    def button_components(self) -> dict:
+        return self.ctx.message.components[0]
+    def author_full(self) ->str:
+        return self.ctx._json["member"]["user"]["username"]+"#"+self.ctx._json["member"]["user"]["discriminator"]
+    def roles(self) -> list:
+        return self.ctx._json["member"]["roles"]
+    def channel_id(self) -> int:
+        return int(self.ctx._json["channel_id"])
         
